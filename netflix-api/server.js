61d754a90c2906b620708eb1294f356c8bf3319d
Subhashisroy <94479675+Subhashisroy@users.noreply.github.com>
2023-01-26 02:23:03 +0530

    Update server.js

--- netflix-api/server.js
+++ netflix-api/server.js
@@ -2,25 +2,27 @@ const express = require("express");
 const cors = require("cors");
 const userRoutes = require("./routes/UserRoutes");
 const mongoose = require("mongoose");
+const dotenv = require("dotenv")
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect("mongodb://localhost:27017/netflix", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => {
-    console.log("DB Connetion Successfull");
-  })
-  .catch((err) => {
-    console.log(err.message);
+
+dotenv.config();
+
+mongoose.connect(process.env.mongo_Url,(err) => {
+  if(err){
+    console.log(err)
+  } else{
+    console.log("mongodb is connected");
+  }
   });
 
 app.use("/api/user", userRoutes);
 
-app.listen(5000, () => {
-  console.log("server started on port 5000");
+const PORT = process.env.PORT || 7000
+app.listen( PORT, () => {
+  console.log(`server started on port ${PORT}`);
 });
